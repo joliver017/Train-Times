09dfe7d6839d267f49abe3bf36db6033cdad2aa2
Oliver 2wist <joliver017@gmail.com>
2019-02-09 17:59:24 -0600

    added comments

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -12,18 +12,23 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+// This submits the fields form when the button is clicked
 $("#fields").on("submit", function(event){
 event.preventDefault();
 
+// These variables are set to whatever is input into the corresponding field
 var name = $("#name").val();
 var destination = $("#destination").val();
 var firstTrain = $("#first-train").val();
 var frequency = $("#frequency").val();
 
+
+// This pushes them to Firebase
 database.ref().push({
     name, destination, firstTrain, frequency
 });
 
+// This clears the field once they are submitted
 $("#name").val("");
 $("#destination").val("");
 $("#first-train").val("");
@@ -31,27 +36,30 @@ $("#frequency").val("");
 
 });
 
+// This now makes variables for each corresponding value in Firebase
 database.ref().on("child_added",function(childSnapshot){
 var name = childSnapshot.val().name;
 var destination = childSnapshot.val().destination;
 var firstTrain = childSnapshot.val().firstTrain;
 var frequency = childSnapshot.val().frequency;
 
-
+// The following code uses moment.js to get the times and find the differences
 var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
-// console.log(firstTrainConverted);
+  // console.log(firstTrainConverted);
 
 var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
-// console.log("DIFFERENCE IN TIME: " + diffTime); 
+  // console.log("DIFFERENCE IN TIME: " + diffTime); 
 
 var tRemainder = diffTime % frequency;
-// console.log(tRemainder);
+  // console.log(tRemainder);
 
 var minutesAway = frequency - tRemainder;
-// console.log("MINUTES TILL TRAIN: " + minutesAway);
+  // console.log("MINUTES TILL TRAIN: " + minutesAway);
 
 var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm a");
-// console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
+  // console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
 
+
+// This appends the variables/table data we want to show up in new rows
 $("#table-data").append("<tr>" + "<td>" + name + "</td> <td>" + destination + "</td> <td>" + frequency + "</td> <td>" + nextArrival + "</td> <td>" + minutesAway + "</td> </tr>");
 });
